2fa5d50795bf11135a6a910131ebca361e3f40d0
Nick <nst447@gmail.com>
2020-01-31 10:13:50 -0800

    Added function to build sql updates

---
+++ server/helpers/partialUpdate.js
@@ -0,0 +1,27 @@
+const sqlForPartialUpdate = (table, items, key, id) => {
+  let idx = 1;
+  let columns = [];
+
+  for (let key in items) {
+    if (key.startsWith("_")) {
+      delete items[key];
+    }
+  }
+
+  for (let column in items) {
+    columns.push(`${column}=$${idx}`);
+    idx += 1;
+  }
+
+  let cols = columns.join(", ");
+  let query = `UPDATE ${table} SET ${cols}
+                WHERE ${key}=$${idx}
+                RETURNING *`;
+  
+  let values = Object.values(items);
+  values.push(id);
+
+  return { query, values };
+}
+
+module.exports = sqlForPartialUpdate;
