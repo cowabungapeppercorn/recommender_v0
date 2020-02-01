d2f0dfe2ec942f023b22ebd38d01a6d16966fb7d
Nick <nst447@gmail.com>
2020-02-01 11:06:11 -0800

    Added get by id method

--- server/models/recommendation.js
+++ server/models/recommendation.js
@@ -3,6 +3,38 @@ const db = require('../db');
 
 class Recommendation {
 
+  static async getById(id) {
+    const result = await db.query(
+      `SELECT r.id, r.user_from, r.user_to, r.content,
+              f.username AS username_from,
+              t.username AS username_to
+        FROM recommendations AS r
+        JOIN users as f ON r.user_from = f.id
+        JOIN users as t ON r.user_to = t.id
+        WHERE r.id = $1`, [id]);
+    const rec = result.rows[0];
+
+    if (!rec) {
+      const notFound = new Error(`The recommendation with an id of '${id}' cannot be found.`);
+      notFound.status = 404;
+      throw notFound;
+    }
+
+    return {
+      id: rec.id,
+      from: {
+        id: rec.user_from,
+        username: rec.username_from
+      },
+      to: {
+        id: rec.user_to,
+        username: rec.username_to
+      },
+      content: rec.content
+    };
+  }
+
+
   static async create({ user_from, user_to, content }) {
     const result = await db.query(
       `INSERT INTO recommendations (
