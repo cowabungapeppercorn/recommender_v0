157c030a1bba0c1772ddf8923420b868400acefa
Nick <nst447@gmail.com>
2020-02-01 11:06:29 -0800

    Added get recommendation route

--- server/routes/recommendations.js
+++ server/routes/recommendations.js
@@ -2,9 +2,29 @@ const Router = require('express').Router;
 const router = new Router();
 const Recommendation = require('../models/recommendation');
 const User = require('../models/user');
+const { ensureLoggedIn } = require('../middleware/auth');
 
 
-router.post('/', async (req, res, next) => {
+router.get('/:id', ensureLoggedIn, async (req, res, next) => {
+  try {
+    const rec = await Recommendation.getById(req.params.id);
+    const user = await User.getByUsername(req.user.username);
+
+    if (rec.user_to === user.id || rec.user_from === user.id) {
+      return res.json(rec);
+    } else {
+      const notAuthorizedErr = new Error("You are not authorized to perform that action.");
+      notAuthorizedErr.status = 401;
+      throw notAuthorizedErr;
+    }
+  }
+  catch (e) {
+    return next(e);
+  }
+});
+
+
+router.post('/', ensureLoggedIn, async (req, res, next) => {
   try {
     const userFrom = await User.getByUsername(req.user.username);
     const userTo = await User.getByUsername(req.body.user_to);
@@ -17,7 +37,7 @@ router.post('/', async (req, res, next) => {
     return res.json(recommendation);
   }
   catch (e) {
-    next(e);
+    return next(e);
   }
 });
 
