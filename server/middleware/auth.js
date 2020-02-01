acdc71af913caccab731b5d2f44b94aef50a16a7
Nick <nst447@gmail.com>
2020-02-01 10:10:34 -0800

    Added ensure correct user middleware function

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -19,13 +19,26 @@ const ensureLoggedIn = (req, res, next) => {
   if (req.user) {
     return next()
   } else {
-    const notAuthorizedErr = new Error("You are not logged in.");
+    const notLoggedInErr = new Error("You are not logged in.");
+    notLoggedInErr.status = 401;
+    throw notLoggedInErr;
+  }
+};
+
+
+const ensureCorrectUser = (req, res, next) => {
+  if (req.user.username === req.params.username) {
+    return next();
+  } else {
+    const notAuthorizedErr = new Error("You are not authorized to perform that action.");
     notAuthorizedErr.status = 401;
     throw notAuthorizedErr;
   }
 };
 
+
 module.exports = {
   authenticateJWT,
-  ensureLoggedIn
+  ensureLoggedIn,
+  ensureCorrectUser
 };
