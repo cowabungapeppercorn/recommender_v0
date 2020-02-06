e6976b1e452505af33536bec7bbb7ba042d93d89
Nick <nst447@gmail.com>
2020-02-06 13:33:24 -0800

    Set up app to start building

---
+++ client/src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <header className="App-header">
+      <h1>Recommender</h1>
+    </header>
+  );
+}
+
+export default App;
